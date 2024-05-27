2e130360b763c9198299655481edd5b60572adde
uchenily <uchenily@qq.com>
2024-05-27 13:48:54 +0800

    test estimation

--- tests/test_estimation.cpp
+++ tests/test_estimation.cpp
@@ -8,15 +8,40 @@ auto test_optimal_parameters() {
     ASSERT_EQ(hashes, 20);
 }
 
+auto test_estimator() {
+    auto estimator = Estimator::create(1000);
+    estimator.incr("key1");
+    estimator.incr("key1");
+    estimator.incr("key1");
+    estimator.incr("key2");
+    LOG_INFO("Frequency of key1: {}", estimator.get("key1"));
+    LOG_INFO("Frequency of key2: {}", estimator.get("key2"));
+    estimator.age(1);
+    LOG_INFO("Frequency of key1 after age(): {}", estimator.get("key1"));
+    LOG_INFO("Frequency of key2 after age(): {}", estimator.get("key2"));
+}
+
+auto test_tiny_lfu() {
+    auto tiny = TinyLfu(1);
+    ASSERT_EQ(tiny.get("x"), 0);
+    ASSERT_EQ(tiny.incr("x"), 1);
+    ASSERT_EQ(tiny.incr("x"), 2);
+    ASSERT_EQ(tiny.get("x"), 2);
+    LOG_INFO("Frequency of x: {}", tiny.get("x"));
+    LOG_INFO("Frequency of y: {}", tiny.get("y"));
+
+    // Might have hash collisions for the others, need to
+    // get() before can assert on the incr() value.
+    auto two = tiny.get("y");
+    ASSERT_EQ(tiny.incr("y"), two + 1);
+    ASSERT_EQ(tiny.incr("y"), two + 2);
+    ASSERT_EQ(tiny.get("y"), two + 2);
+    LOG_INFO("Frequency of x: {}", tiny.get("x"));
+    LOG_INFO("Frequency of y: {}", tiny.get("y"));
+}
+
 auto main() -> int {
     test_optimal_parameters();
-    // // auto estimator = Estimator::create(1000);
-    // auto estimator = Estimator::create(10);
-    // estimator.incr("key1");
-    // estimator.incr("key1");
-    // estimator.incr("key1");
-    // estimator.incr("key2");
-    // LOG_INFO("Frequency of key1: {}", estimator.get("key1"));
-    // LOG_INFO("Frequency of key2: {}", estimator.get("key2"));
-    // estimator.age(1);
+    test_estimator();
+    test_tiny_lfu();
 }
